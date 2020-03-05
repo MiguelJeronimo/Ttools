a9171c924b66b21e79966c6af10e75f488efde5e
Miguel Angel Jerónimo Díaz <miguelangeljd93@gmail.com>
2020-03-04 23:47:28 -0600

    1.- Se asignaron los colores a los switches de la intefaz 2.- Se creo la clase calcularBlessings para tener en ella las operaciones y mandarlas llamar en la actividad principal. 2.- Se desarrollo la logica de la interfaz de las blessins, calculando las blessings mediante el evento del boton Calcular. 3.- Se calculan 2 blessings opcionales mediante el uso del evento onClick que son aplicados en los Switches.

--- app/src/main/java/com/example/ttools/MainActivity.java
+++ app/src/main/java/com/example/ttools/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ttools;
 
 import android.os.Bundle;
 
+import com.example.ttools.Operaciones.calcularBlessings;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -11,8 +12,25 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+/** Declaracion de los objetos de los componentes**/
+
+    TextView spiritual, embrace, suns, solitude, phoenix, twits_of_fate, heart, blood,total;
+
+    EditText nivel;
+
+    Button calcular;
+
+    Switch swtich_heart, swtich_blood;
+
+    calcularBlessings c = new calcularBlessings();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +39,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /**Instanciaremos los objetos de los componentes**/
+
+        nivel = (EditText)findViewById(R.id.Nivel);
+
+        spiritual = (TextView)findViewById(R.id.spiritual);
+        embrace = (TextView)findViewById(R.id.Embrace);
+        suns = (TextView)findViewById(R.id.Suns);
+        solitude = (TextView)findViewById(R.id.Solicitude);
+        phoenix = (TextView)findViewById(R.id.Phoenix);
+        twits_of_fate = (TextView)findViewById(R.id.Twits);
+        heart = (TextView)findViewById(R.id.Heart_mountain);
+        blood = (TextView)findViewById(R.id.Blood_moutain);
+        total = (TextView)findViewById(R.id.Total);
+
+        /**Boton y Switches**/
+
+
+        calcular = (Button)findViewById(R.id.calcular);
+        calcular.setOnClickListener(this);
+
+        swtich_heart = (Switch)findViewById(R.id.switch1);
+        swtich_heart.setOnClickListener(this);
+
+        swtich_blood = (Switch)findViewById(R.id.switch2);
+        swtich_blood.setOnClickListener(this);
+
+     /**   FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });**/
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,4 +97,101 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.calcular){
+
+            String nivel_personaje =  nivel.getText().toString();
+
+            if (nivel_personaje.equalsIgnoreCase("")){
+                Toast.makeText(this,"Favor de ingresar el nivel",Toast.LENGTH_SHORT).show();
+            } else{
+                int Nivel = Integer.parseInt(nivel_personaje);
+
+                if (Nivel <= 30){
+                    Toast.makeText(this, "Recuerda que si tu nivel es menor o igual a 30 tus bless valen 10K en total", Toast.LENGTH_SHORT).show();
+                } else if (Nivel>=31&&Nivel<120){
+
+                    // Blessings individual (costo unitario de las bless principales y twits of fate)
+                    int bless = c.blessingIndividual(Nivel);
+
+                    //calcularemos la suma de las  blessins princiapales
+                    int suma_blessings_principales = c.sumaBlessingsPrincipales(Nivel);
+
+                    //Precio unitario de la bless especial
+                    //int bless_especial = c.blessingEspecial(Nivel);
+
+                    spiritual.setText(Integer.toString(bless));
+                    embrace.setText(Integer.toString(bless));
+                    suns.setText(Integer.toString(bless));
+                    solitude.setText(Integer.toString(bless));
+                    phoenix.setText(Integer.toString(bless));
+                    twits_of_fate.setText(Integer.toString(bless));
+                    total.setText(Integer.toString( suma_blessings_principales + bless));
+                    nivel.setText("");
+                } else{
+                    Toast.makeText(this, "Recuerda que apartir del nivel 120 tus bless empiezan a valen 100K en total",Toast.LENGTH_LONG).show();
+                }
+            }
+
+
+
+        } else if (swtich_blood.isChecked()){//switch de Heart of muntain esta checkeado
+            String nivel_personaje =  nivel.getText().toString();
+
+            if (nivel_personaje.equalsIgnoreCase("")){
+                Toast.makeText(this,"Favor de ingresar el nivel",Toast.LENGTH_SHORT).show();
+            } else{
+
+                int Nivel = Integer.parseInt(nivel_personaje);
+                int bless_especial = c.blessingEspecial(Nivel);
+                blood.setText(Integer.toString(bless_especial));
+            }
+
+
+        } else if (swtich_heart.isChecked()){// swtich de Blood of mountains esta checkeado.
+
+            String nivel_personaje =  nivel.getText().toString();
+            if (nivel_personaje.equalsIgnoreCase("")){
+                Toast.makeText(this,"Favor de ingresar el nivel",Toast.LENGTH_SHORT).show();
+            } else {
+
+                int Nivel = Integer.parseInt(nivel_personaje);
+                int bless_especial = c.blessingEspecial(Nivel);
+                heart.setText(Integer.toString(bless_especial));
+            }
+
+
+
+        }
+
+
+    }
+
+   /** @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if (swtich_heart.isChecked()){
+
+            String nivel_personaje =  nivel.getText().toString();
+            int Nivel = Integer.parseInt(nivel_personaje);
+            int bless_especial = c.blessingEspecial(Nivel);
+
+            heart.setText(Integer.toString(bless_especial));
+
+        } else if (swtich_blood.isChecked()){
+            String nivel_personaje =  nivel.getText().toString();
+            int Nivel = Integer.parseInt(nivel_personaje);
+            int bless_especial = c.blessingEspecial(Nivel);
+
+            blood.setText(Integer.toString(bless_especial));
+        }
+
+
+        }**/
+
+
+    }
+
