2113615036dea0cc2a76c185d27e699e14b8e330
Miguel Angel Jerónimo Díaz <miguelangeljd93@gmail.com>
2020-03-04 23:59:44 -0600

    1.- Se asignaron los colores a los switches de la intefaz 2.- Se creo la clase calcularBlessings para tener en ella las operaciones y mandarlas llamar en la actividad principal. 3.- Se desarrollo la logica de la interfaz de las blessins, calculando las blessings mediante el evento del boton Calcular. 4.- Se calculan 2 blessings opcionales mediante el uso del evento onClick que son aplicados en los Switches.

--- app/src/main/java/com/example/ttools/Operaciones/calcularBlessings.java
+++ app/src/main/java/com/example/ttools/Operaciones/calcularBlessings.java
@@ -2,39 +2,7 @@ package com.example.ttools.Operaciones;
 
 public class calcularBlessings {
 
-    int nivel;
 
-    public void calcularBlessings(int nivel){
-            this.nivel=nivel;
-    }
-
-    public void calcularBlessings(){
-
-    }
-
-
-
-    public int blessingIndividual (int nivel){ //Calculo de las blessings principales individualmente (el precio aplica tambien para la twits of fate)
-
-         int constante = 200;
-         int blessing = constante*(nivel - 20);
-         return blessing;
-    }
-
-    public int blessingEspecial(int nivel){  // calculo de las blessings especiales (Heart of the Mountain y 	Blood of the Mountain) individualmente
-
-        int constante = 260;
-        int blessing_especial = constante *(nivel -20);
-        return blessing_especial;
-
-    }
-
-    public int sumaBlessingsPrincipales(int nivel){  // es la suma de las cinco blessings principales sin la twits of fate
-
-        int suma_blessings_principales = blessingIndividual(nivel)*5;  //suando las blessings principales
-        return suma_blessings_principales;
-
-    }
 
 
 
