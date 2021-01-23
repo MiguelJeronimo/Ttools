64e9a25b674d6f8ff9d356ff34c0cfdbd1bc255b
Miguel Angel Jerónimo <miguelangeljd93@gmail.com>
2021-01-22 23:25:54 -0600

    En el AndroidManifest.xml se incluyo el permiso de internet. Se implemento en la interfaz de characters.java la parte para consumir la API publica de Tibia online utilizando la libreria Volley.

--- app/src/main/java/com/example/ttools/characters.java
+++ app/src/main/java/com/example/ttools/characters.java
@@ -2,6 +2,11 @@ package com.example.ttools;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
@@ -15,6 +20,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class characters extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,7 +40,7 @@ public class characters extends AppCompatActivity implements View.OnClickListene
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Aparicion del boton regresar en el action bar
 
-
+        //inicializando los componentes de la interfaz
         nombre_persona = (EditText) findViewById(R.id.editTextTextPersonName);
         nombre = (TextView)findViewById(R.id.nombre);
         titulo = (TextView) findViewById(R.id.titulo);
@@ -82,7 +90,166 @@ public class characters extends AppCompatActivity implements View.OnClickListene
     public void onClick(View v) {
 
         if (v.getId() == R.id.btnenviar){
+            final String nombre_personaje = nombre_persona.getText().toString();
+            String a [] = nombre_personaje.split(" ");
+            // System.out.println(a[0]+"%20"+a[1]);
+            String nombrep = a[0];
+            //String apellido = a[1];
 
+
+            final RequestQueue requestQueue = Volley.newRequestQueue(characters.this);
+            //Validaremos el campo del nombre
+            String url = null;
+
+
+            if (nombre_personaje.equalsIgnoreCase("")){
+                // Toast.makeText(this, "Ingrese el nombre del personaje", Toast.LENGTH_SHORT).show();
+
+                Snackbar.make(null,"Ingrese el nombre de personaje", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+
+            } else if(a.length == 1) {
+                url = "https://api.tibiadata.com/v2/characters/"+nombre_personaje+"%20.json";}
+            else if(a.length==3){
+                url = "https://api.tibiadata.com/v2/characters/"+nombrep+"%20"+a[1]+"%20"+a[2]+".json";
+            }
+
+            else {
+
+                url = "https://api.tibiadata.com/v2/characters/"+nombrep+"%20"+a[1]+".json";
+            }
+
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, new com.android.volley.Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+
+                            try {
+                                // text.setText();
+
+                                String snombre_persona;
+                                String stitulo;
+                                String ssexo;
+                                String svocacion;
+                                String snivel;
+                                String sarchiviement;
+                                String smundo;
+                                String sresidencia;
+                                String sguild;
+                                String slastlogin;
+                                String scomentario;
+                                String zonahoraria;
+                                String rango;
+                                boolean serror;
+                                snombre_persona = response.getJSONObject("characters").getJSONObject("data").getString("name").toString();
+                                stitulo = response.getJSONObject("characters").getJSONObject("data").getString("title").toString();
+                                ssexo = response.getJSONObject("characters").getJSONObject("data").getString("sex").toString();
+                                svocacion = response.getJSONObject("characters").getJSONObject("data").getString("vocation").toString();
+                                snivel = response.getJSONObject("characters").getJSONObject("data").getString("level").toString();
+                                sarchiviement = response.getJSONObject("characters").getJSONObject("data").getString("achievement_points").toString();
+                                smundo = response.getJSONObject("characters").getJSONObject("data").getString("world").toString();
+                                sresidencia = response.getJSONObject("characters").getJSONObject("data").getString("residence").toString();
+                                slastlogin = response.getJSONObject("characters").getJSONObject("data").getJSONArray("last_login").getJSONObject(0).get("date").toString();
+
+                                // serror = response.getJSONObject("characters").getJSONObject("data").has("guild");
+                                if (response.getJSONObject("characters").getJSONObject("data").optString("comment") == "" && response.getJSONObject("characters").getJSONObject("data").optString("guild") == "") {//en caso de que no tenga guild ni comentarios
+                                    guild.setText("No tiene guild");
+                                    comentario.setText("No tiene comentario");
+                                    nombre.setText(snombre_persona);
+                                    titulo.setText(stitulo);
+                                    sexo.setText(ssexo);
+                                    vocacion.setText(svocacion);
+                                    nivel.setText(snivel);
+                                    archiviement.setText(sarchiviement);
+                                    mundo.setText(smundo);
+                                    residencia.setText(sresidencia);
+                                    lastlogin.setText(slastlogin);
+
+                                } else if (response.getJSONObject("characters").getJSONObject("data").optString("comment") == ""){//en caso de que no tenga comentario
+                                    guild.setText(response.getJSONObject("characters").getJSONObject("data").getJSONObject("guild").getString("rank")+" of the "+response.getJSONObject("characters").getJSONObject("data").getJSONObject("guild").getString("name"));
+
+                                    comentario.setText("");
+                                    nombre.setText(snombre_persona);
+                                    titulo.setText(stitulo);
+                                    sexo.setText(ssexo);
+                                    vocacion.setText(svocacion);
+                                    nivel.setText(snivel);
+                                    archiviement.setText(sarchiviement);
+                                    mundo.setText(smundo);
+                                    residencia.setText(sresidencia);
+                                    lastlogin.setText(slastlogin);
+                                } else if (response.getJSONObject("characters").getJSONObject("data").optString("guild") == ""){// EN CASO DE QUE NO TENGA COMENTARIO NI GUILD
+                                    guild.setText("No tiene guild");
+                                    comentario.setText(response.getJSONObject("characters").getJSONObject("data").getString("comment"));
+                                    nombre.setText(snombre_persona);
+                                    titulo.setText(stitulo);
+                                    sexo.setText(ssexo);
+                                    vocacion.setText(svocacion);
+                                    nivel.setText(snivel);
+                                    archiviement.setText(sarchiviement);
+                                    mundo.setText(smundo);
+                                    residencia.setText(sresidencia);
+                                    lastlogin.setText(slastlogin);
+                                }
+
+                                else{
+
+
+                                    guild.setText(response.getJSONObject("characters").getJSONObject("data").getJSONObject("guild").getString("rank")+" of the "+response.getJSONObject("characters").getJSONObject("data").getJSONObject("guild").getString("name"));
+                                    // sguild = response.getJSONObject("characters").getJSONObject("data").getJSONObject("guild").getString("name");
+                                    //guild.setText(sguild);
+                                    comentario.setText(response.getJSONObject("characters").getJSONObject("data").getString("comment"));
+                                    nombre.setText(snombre_persona);
+                                    titulo.setText(stitulo);
+                                    sexo.setText(ssexo);
+                                    vocacion.setText(svocacion);
+                                    nivel.setText(snivel);
+                                    archiviement.setText(sarchiviement);
+                                    mundo.setText(smundo);
+                                    residencia.setText(sresidencia);
+                                    lastlogin.setText(slastlogin);
+
+                                }
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                //e.getMessage();
+
+                                Snackbar.make(null, "El personaje no existe.", Snackbar.LENGTH_LONG)
+                                        .setAction("Action", null).show();
+                                guild.setText("");
+                                // sguild = response.getJSONObject("characters").getJSONObject("data").getJSONObject("guild").getString("name");
+                                //guild.setText(sguild);
+                                comentario.setText("");
+                                nombre.setText("");
+                                titulo.setText("");
+                                sexo.setText("");
+                                vocacion.setText("");
+                                nivel.setText("");
+                                archiviement.setText("");
+                                mundo.setText("");
+                                residencia.setText("");
+                                lastlogin.setText("");
+                            }
+                            //  String respuesta = gson.toJson(response);
+
+
+
+                            // text.setText();
+
+
+                        }
+                    }, new com.android.volley.Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // text.setText(error.toString());
+                        }
+                    });
+
+            requestQueue.add(jsonObjectRequest);
 
 
         }
