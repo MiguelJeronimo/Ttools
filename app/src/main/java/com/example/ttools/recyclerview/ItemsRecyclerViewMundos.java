1697782acb18d014533aae6267e0455fcda88a1b
Miguel Jeronimo <miguelangeljd93@gmail.com>
2022-05-06 14:15:46 -0500

    Se creo el modelo de los items del recyclerview mundos

---
+++ app/src/main/java/com/example/ttools/recyclerview/ItemsRecyclerViewMundos.java
@@ -0,0 +1,97 @@
+package com.example.ttools.recyclerview;
+
+public class ItemsRecyclerViewMundos {
+
+        String lbName, lbStatus, lbPlayersOnline;
+        String lbLocation, lbPvpType, lbPremiumOnly;
+        String lbTransferType, lbBattleyeProtected, lbBattleyeDate;
+        String lbGameWorlType, lbTournamentWorldType;
+
+        public String getLbName() {
+                return lbName;
+        }
+
+        public void setLbName(String lbName) {
+                this.lbName = lbName;
+        }
+
+        public String getLbStatus() {
+                return lbStatus;
+        }
+
+        public void setLbStatus(String lbStatus) {
+                this.lbStatus = lbStatus;
+        }
+
+        public String getLbPlayersOnline() {
+                return lbPlayersOnline;
+        }
+
+        public void setLbPlayersOnline(String lbPlayersOnline) {
+                this.lbPlayersOnline = lbPlayersOnline;
+        }
+
+        public String getLbLocation() {
+                return lbLocation;
+        }
+
+        public void setLbLocation(String lbLocation) {
+                this.lbLocation = lbLocation;
+        }
+
+        public String getLbPvpType() {
+                return lbPvpType;
+        }
+
+        public void setLbPvpType(String lbPvpType) {
+                this.lbPvpType = lbPvpType;
+        }
+
+        public String getLbPremiumOnly() {
+                return lbPremiumOnly;
+        }
+
+        public void setLbPremiumOnly(String lbPremiumOnly) {
+                this.lbPremiumOnly = lbPremiumOnly;
+        }
+
+        public String getLbTransferType() {
+                return lbTransferType;
+        }
+
+        public void setLbTransferType(String lbTransferType) {
+                this.lbTransferType = lbTransferType;
+        }
+
+        public String getLbBattleyeProtected() {
+                return lbBattleyeProtected;
+        }
+
+        public void setLbBattleyeProtected(String lbBattleyeProtected) {
+                this.lbBattleyeProtected = lbBattleyeProtected;
+        }
+
+        public String getLbBattleyeDate() {
+                return lbBattleyeDate;
+        }
+
+        public void setLbBattleyeDate(String lbBattleyeDate) {
+                this.lbBattleyeDate = lbBattleyeDate;
+        }
+
+        public String getLbGameWorlType() {
+                return lbGameWorlType;
+        }
+
+        public void setLbGameWorlType(String lbGameWorlType) {
+                this.lbGameWorlType = lbGameWorlType;
+        }
+
+        public String getLbTournamentWorldType() {
+                return lbTournamentWorldType;
+        }
+
+        public void setLbTournamentWorldType(String lbTournamentWorldType) {
+                this.lbTournamentWorldType = lbTournamentWorldType;
+        }
+}
