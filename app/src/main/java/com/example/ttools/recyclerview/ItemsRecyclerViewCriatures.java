775afc64b4611a728e23d323b98a9ad88bd8605f
Miguel Jeronimo <miguelangeljd93@gmail.com>
2022-05-24 15:31:37 -0500

    Modelo del recyclerview criaturas

---
+++ app/src/main/java/com/example/ttools/recyclerview/ItemsRecyclerViewCriatures.java
@@ -0,0 +1,35 @@
+package com.example.ttools.recyclerview;
+
+public class ItemsRecyclerViewCriatures {
+    String lbName, lbrace, imagerace;
+
+    public ItemsRecyclerViewCriatures(String lbName, String lbrace, String imagerace) {
+        this.lbName = lbName;
+        this.lbrace = lbrace;
+        this.imagerace = imagerace;
+    }
+
+    public String getLbName() {
+        return lbName;
+    }
+
+    public void setLbName(String lbName) {
+        this.lbName = lbName;
+    }
+
+    public String getLbrace() {
+        return lbrace;
+    }
+
+    public void setLbrace(String lbrace) {
+        this.lbrace = lbrace;
+    }
+
+    public String getImagerace() {
+        return imagerace;
+    }
+
+    public void setImagerace(String imagerace) {
+        this.imagerace = imagerace;
+    }
+}
