34899e1af0dc7d4e0d19e4d717cc3f48213a37ac
MiguelJeronimo <mjeronimo@frd.org.mx>
2022-12-02 09:20:36 -0600

    Ajustando contenido y el boton regresar

--- app/src/main/java/com/example/ttools/GuildInformationName.java
+++ app/src/main/java/com/example/ttools/GuildInformationName.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.navigation.NavController;
@@ -23,16 +27,41 @@ public class GuildInformationName extends AppCompatActivity {
     private ActivityGuildInformationNameBinding binding;
     Intent intent;
     String guildName;
+    ImageView imageViewGuildLogo;
+    TextView textViewGuildName,textViewDescription,textViewInWar,textViewOnline,
+             textViewNombre,textViewMundo,textViewPiad,textViewFounded,textViewActive;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityGuildInformationNameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Aparicion del boton regresar en el action bar
         //respuesta de la pantalla anterior
         intent = getIntent();
         guildName = intent.getStringExtra("nameGuild");
-        Toast.makeText(this,guildName,Toast.LENGTH_SHORT).show();
+        imageViewGuildLogo = binding.getRoot().findViewById(R.id.imageViewLogo);
+        textViewGuildName = binding.getRoot().findViewById(R.id.textViewGuildName);
+        textViewDescription = binding.getRoot().findViewById(R.id.textViewDescription);
+        textViewInWar = binding.getRoot().findViewById(R.id.textViewInWar);
+        textViewOnline = binding.getRoot().findViewById(R.id.textViewOnline);
+        textViewNombre = binding.getRoot().findViewById(R.id.textViewNombre);
+        textViewMundo = binding.getRoot().findViewById(R.id.textViewMundo);
+        textViewPiad = binding.getRoot().findViewById(R.id.textViewPiad);
+        textViewFounded = binding.getRoot().findViewById(R.id.textViewFounded);
+        textViewActive = binding.getRoot().findViewById(R.id.textViewActive);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) { //aqui daremos el evento al boton regresar de nuestro action bar, haciendo uso de los ids del sistema android
+            finish();// finalizamos la actividad
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
