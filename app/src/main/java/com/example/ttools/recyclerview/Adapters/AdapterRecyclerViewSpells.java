f24b6f92599d398c18ec0ccd90124f246f084840
MiguelJeronimo <mjeronimo@frd.org.mx>
2022-12-07 10:18:35 -0600

    Modelado y Adaptador del RecyclerView de las spells

---
+++ app/src/main/java/com/example/ttools/recyclerview/Adapters/AdapterRecyclerViewSpells.java
@@ -0,0 +1,66 @@
+package com.example.ttools.recyclerview.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ttools.R;
+import com.example.ttools.recyclerview.ItemsRecyclerViewSpells;
+
+import java.util.List;
+
+public class AdapterRecyclerViewSpells extends RecyclerView.Adapter<AdapterRecyclerViewSpells.ViewHolder> {
+    List<ItemsRecyclerViewSpells> items_spells;
+
+    public AdapterRecyclerViewSpells(List<ItemsRecyclerViewSpells> items_spells){
+        this.items_spells = items_spells;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_spells_tibia,parent,false);
+        return new ViewHolder(vista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final ItemsRecyclerViewSpells itemsRecyclerViewSpells = (ItemsRecyclerViewSpells) items_spells.get(position);
+        holder.lbName.setText(itemsRecyclerViewSpells.getNombre());
+        holder.lbFormula.setText(itemsRecyclerViewSpells.getFormula());
+        holder.lbNivel.setText(itemsRecyclerViewSpells.getLevel());
+        holder.lbMana.setText(itemsRecyclerViewSpells.getMana());
+        holder.lbPrecio.setText(itemsRecyclerViewSpells.getPrecio());
+        holder.lbType.setText(itemsRecyclerViewSpells.getTipo());
+        holder.lbGrupo.setText(itemsRecyclerViewSpells.getGrupo());
+        holder.lbSpell_Id.setText(itemsRecyclerViewSpells.getSpellId());
+        holder.lbPremium.setText(itemsRecyclerViewSpells.getPremium());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items_spells.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView lbName, lbFormula, lbNivel, lbMana, lbPrecio, lbType,
+        lbGrupo, lbSpell_Id, lbPremium;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            lbName = itemView.findViewById(R.id.textViewName);
+            lbFormula = itemView.findViewById(R.id.textViewFormula);
+            lbNivel = itemView.findViewById(R.id.textViewNivvel);
+            lbMana = itemView.findViewById(R.id.textViewMana);
+            lbPrecio = itemView.findViewById(R.id.textViewPrecio);
+            lbType = itemView.findViewById(R.id.textViewType);
+            lbGrupo = itemView.findViewById(R.id.textViewGroup);
+            lbSpell_Id = itemView.findViewById(R.id.textViewSpell_Id);
+            lbPremium = itemView.findViewById(R.id.textViewPremium);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ttools/recyclerview/ItemsRecyclerViewSpells.java
@@ -0,0 +1,106 @@
+package com.example.ttools.recyclerview;
+
+public class ItemsRecyclerViewSpells {
+    private String nombre;
+    private String formula;
+    private String mana;
+    private String precio;
+    private String tipo;
+    private String grupo;
+    private String spellid;
+    private String premium;
+    private String level;
+
+    public ItemsRecyclerViewSpells(
+            String nombre,
+            String formula,
+            String mana,
+            String precio,
+            String tipo,
+            String grupo,
+            String spellid,
+            String premium,
+            String level){
+        this.nombre=nombre;
+        this.formula=formula;
+        this.mana=mana;
+        this.precio=precio;
+        this.tipo=tipo;
+        this.grupo=grupo;
+        this.spellid=spellid;
+        this.premium=premium;
+        this.level=level;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFormula() {
+        return formula;
+    }
+
+    public void setFormula(String formula) {
+        this.formula = formula;
+    }
+
+    public String getMana() {
+        return mana;
+    }
+
+    public void setMana(String mana) {
+        this.mana = mana;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getSpellId() {
+        return spellid;
+    }
+
+    public void setSpellId(String spellid) {
+        this.spellid = spellid;
+    }
+
+    public String getPremium() {
+        return premium;
+    }
+
+    public void setPremium(String premium) {
+        this.premium = premium;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+}
