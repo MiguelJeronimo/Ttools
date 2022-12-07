719ef729b2fc6504b294e0659d53d9bbd1270700
MiguelJeronimo <mjeronimo@frd.org.mx>
2022-12-07 17:10:34 -0600

    Implementación del boton de regresar

--- app/src/main/java/com/example/ttools/SpellInformationActivity.java
+++ app/src/main/java/com/example/ttools/SpellInformationActivity.java
@@ -2,8 +2,10 @@ package com.example.ttools;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ttools.databinding.ActivitySpellInformationBinding;
@@ -19,9 +21,19 @@ public class SpellInformationActivity extends AppCompatActivity {
         binding = ActivitySpellInformationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Aparicion del boton regresar en el action bar
         intent = getIntent();
         id_spell = intent.getStringExtra("ID");
         Toast.makeText(this, "El id es: "+id_spell, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) { //aqui daremos el evento al boton regresar de nuestro action bar, haciendo uso de los ids del sistema android
+            finish();// finalizamos la actividad
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
