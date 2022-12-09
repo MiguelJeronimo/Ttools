f6edaff7968a035883e09060a5c56141b5284237
MiguelJeronimo <mjeronimo@frd.org.mx>
2022-12-09 13:15:38 -0600

    Moviendo la funcion blessings de la pantalla principal a otra pantalla

--- app/src/main/java/com/example/ttools/MainActivity.java
+++ app/src/main/java/com/example/ttools/MainActivity.java
@@ -23,14 +23,6 @@ import android.widget.Toast;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, MenuItem.OnMenuItemClickListener {
-/** Declaracion de los objetos de los componentes**/
-    TextView spiritual, embrace, suns, solitude, phoenix, twits_of_fate, heart, blood,total;
-    EditText nivel;
-    Button calcular;
-    Switch swtich_heart, swtich_blood;
-    calcularBlessings c = new calcularBlessings();
-    // para formatear numeros a formato de dinero.
-    DecimalFormat decimalFormat = new DecimalFormat("###,###.00");
     /**
      * Navigation Drawer
      * */
@@ -60,27 +52,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         navigationView.getMenu().findItem(R.id.nd_mundos).setOnMenuItemClickListener(this);
         navigationView.getMenu().findItem(R.id.nd_guilds).setOnMenuItemClickListener(this);
         navigationView.getMenu().findItem(R.id.nd_spells).setOnMenuItemClickListener(this);
+        navigationView.getMenu().findItem(R.id.nd_bless).setOnMenuItemClickListener(this);
 
-        /**Instanciaremos los objetos de los componentes**/
-        nivel = (EditText)findViewById(R.id.Nivel);
-        spiritual = (TextView)findViewById(R.id.spiritual);
-        embrace = (TextView)findViewById(R.id.Embrace);
-        suns = (TextView)findViewById(R.id.Suns);
-        solitude = (TextView)findViewById(R.id.Solicitude);
-        phoenix = (TextView)findViewById(R.id.Phoenix);
-        twits_of_fate = (TextView)findViewById(R.id.Twits);
-        heart = (TextView)findViewById(R.id.Heart_mountain);
-        blood = (TextView)findViewById(R.id.Blood_moutain);
-        total = (TextView)findViewById(R.id.Total);
-        /**Boton y Switches**/
-        calcular = (Button)findViewById(R.id.calcular);
-        calcular.setOnClickListener(this);
-
-        swtich_heart = (Switch)findViewById(R.id.switch1);
-        swtich_heart.setOnClickListener(this);
-
-        swtich_blood = (Switch)findViewById(R.id.switch2);
-        swtich_blood.setOnClickListener(this);
     }
 
 
@@ -124,73 +97,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        if (v.getId() == R.id.calcular){
-            String nivel_personaje =  nivel.getText().toString();
-            if (nivel_personaje.equalsIgnoreCase("")){
-                Toast.makeText(this,"Favor de ingresar el nivel",Toast.LENGTH_SHORT).show();
-            } else{
-                int Nivel = Integer.parseInt(nivel_personaje);
-                    // Blessings individual (costo unitario de las bless principales y twits of fate)
-                    c.blessingIndividual(Nivel);
-                    //Suma de las 5 bless principales
-                    c.sumaBlessingsPrincipales(Nivel);
-                    //formateamos el costo a formato de moneda
-                    String blessing_individual = decimalFormat.format(c.getBlessingIndividual());
-                    String total_blessings_principales = decimalFormat.format(c.getSumaBlessingsPrincipales() + c.getBlessingIndividual());
-                    /**UNA VEZ VALIDADO EL CAMPO DE TEXTO, IMPRIMIMOS LOS RESULTADOS EN LAS ETIQUETAS CORRESPONDIENTES**/
-                    spiritual.setText(blessing_individual);
-                    embrace.setText(blessing_individual);
-                    suns.setText(blessing_individual);
-                    solitude.setText(blessing_individual);
-                    phoenix.setText(blessing_individual);
-                    twits_of_fate.setText(blessing_individual);
-            }
-
-        }
-
-        if (swtich_blood.isChecked()){//switch de Heart of muntain esta checkeado
-            String nivel_personaje =  nivel.getText().toString();
-            if (nivel_personaje.equalsIgnoreCase("")){
-                Toast.makeText(this,"Favor de ingresar el nivel",Toast.LENGTH_SHORT).show();
-            } else {
-                int Nivel = Integer.parseInt(nivel_personaje);
-                c.blessingEspecial(Nivel);
-                int get_especial = c.getBlessingEspecial();
-                c.setHeartOfMountain(get_especial);
-                //formateamos el costo a formato de moneda
-                String blessing_especial = decimalFormat.format(c.getBlessingEspecial());
-                blood.setText(blessing_especial);
-            }
-        } else {
-            //hacer en caso de que no este checkiado
-            blood.setText("");
-            c.setHeartOfMountain(0);
-        }
-
-
-        if (swtich_heart.isChecked()){// swtich de Blood of mountains esta checkeado.
-            String nivel_personaje =  nivel.getText().toString();
-            if (nivel_personaje.equalsIgnoreCase("")){
-                Toast.makeText(this,"Favor de ingresar el nivel",Toast.LENGTH_SHORT).show();
-            } else {
-                int Nivel = Integer.parseInt(nivel_personaje);
-                c.blessingEspecial(Nivel);
-                int get_especial = c.getBlessingEspecial();
-                c.setBloodOfMountain(get_especial);
-                //formateamos el costo a formato de moneda
-                String blessing_especial = decimalFormat.format(c.getBlessingEspecial());
-                heart.setText(blessing_especial);
-
-            }
-        } else{
-            //hacer en caso de que no este chequiado
-            heart.setText("");
-            c.setBloodOfMountain(0);
-        }
-
-        String total_blessing = decimalFormat.format(c.getSumaBlessingsPrincipales() + c.getBlessingIndividual()+c.getHeartOfMountain()+c.getBloodOfMountain());
-        // imprimiendo los totales en las etiquetas de total de blessings opcionales y el total de todas las blessings
-        total.setText(total_blessing);
     }
 
     @Override
@@ -223,6 +129,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (menuItem.getItemId() == R.id.nd_spells){
             Intent spellsTibia = new Intent(this, Spells_Tibia.class);
             startActivity(spellsTibia);
+            drawerLayout.close();
+        }else if (menuItem.getItemId() == R.id.nd_bless){
+            Intent blessTibia= new Intent(this, Blessings.class);
+            startActivity(blessTibia);
             drawerLayout.close();
         }
         return false;
