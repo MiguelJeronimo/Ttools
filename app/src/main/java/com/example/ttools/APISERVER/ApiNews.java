4b42a114d67f9cf1902648b535e60c845c292f3e
MiguelJeronimo <mjeronimo@frd.org.mx>
2022-12-16 11:37:42 -0600

    Modelo de datos para consumir las noticias del API

---
+++ app/src/main/java/com/example/ttools/APISERVER/ApiNews.java
@@ -0,0 +1,13 @@
+package com.example.ttools.APISERVER;
+
+import com.example.ttools.APISERVER.models.News.News;
+
+import java.util.ArrayList;
+
+public class ApiNews {
+    ArrayList<News> news = new ArrayList<>();
+
+    public ArrayList<News> getNews() {
+        return news;
+    }
+}
---
+++ app/src/main/java/com/example/ttools/APISERVER/models/News/News.java
@@ -0,0 +1,29 @@
+package com.example.ttools.APISERVER.models.News;
+
+public class News {
+    String id, date, news, category, type, url;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
